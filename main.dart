354df9a1d320aaa3b0a25c640935485004b454c3
elaman <73353291+Elamantoktoshev@users.noreply.github.com>
2021-10-07 20:16:59 +0600

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.brown,
+        appBar: AppBar(
+          backgroundColor: Colors.amberAccent,
+          centerTitle: true,
+          title: Text('I am Rich!'),
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('assets/images/diamond.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
